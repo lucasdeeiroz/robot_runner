977de5b6277d66140093da35a444c4acef288764
Lucas de Eiroz Rodrigues <lucas.de.eiroz@gmail.com>
2026-07-29 00:03:47 -0300

    Bump version to v3.0.0-alpha2

--- companion/app/build.gradle.kts
+++ companion/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 300
-        versionName = "3.0.0-alpha1"
+        versionName = "3.0.0-alpha2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
